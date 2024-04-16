ab98f31411c599127d18ddeb58d213abbb888bd9
marius sandulache <mariussandulache2015@gmail.com>
2024-04-16 17:13:15 +0300

    adding seatchart.js

--- app/assets/js/custom.js
+++ app/assets/js/custom.js
@@ -2,3 +2,24 @@ require('modal-video');
 const ModalVideo = require("modal-video/lib/core");
 
 new ModalVideo('.js-modal-btn');
+
+const Seatchart = require('seatchart/dist/seatchart.min');
+
+
+var element = document.getElementById('container');
+var options = {
+    map: {
+        rows: 7,
+        columns: 7,
+        seatTypes: {
+            default: {
+                label: 'Economy',
+                cssClass: 'economy',
+                price: 10,
+            },
+        },
+    },
+};
+
+var sc = new Seatchart(element, options);
+
