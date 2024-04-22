378dd4ef0354b4d56344fcf459e6fb51ae523646
marius sandulache <mariussandulache2015@gmail.com>
2024-04-22 14:16:50 +0300

    custom seats

--- app/assets/js/custom.js
+++ app/assets/js/custom.js
@@ -5,21 +5,46 @@ new ModalVideo('.js-modal-btn');
 
 const Seatchart = require('seatchart/dist/seatchart.min');
 
-
-var element = document.getElementById('container');
 var options = {
     map: {
-        rows: 7,
-        columns: 7,
+        rows: 10,
+        columns: 10,
         seatTypes: {
             default: {
                 label: 'Economy',
                 cssClass: 'economy',
+                price: 15,
+            },
+            first: {
+                label: 'First Class',
+                cssClass: 'first-class',
+                price: 25,
+                seatRows: [0, 1, 2],
+            },
+            reduced: {
+                label: 'Reduced',
+                cssClass: 'reduced',
                 price: 10,
+                seatRows: [7, 8, 9],
             },
         },
+        disabledSeats: [
+            { row: 0, col: 0 },
+            { row: 0, col: 9 },
+        ],
+        reservedSeats: [
+            { row: 0, col: 3 },
+            { row: 0, col: 4 },
+        ],
+        selectedSeats: [{ row: 0, col: 5 }, { row: 0, col: 6 }],
+        rowSpacers: [3, 7],
+        columnSpacers: [5],
     },
 };
 
+var element = document.getElementById('container');
 var sc = new Seatchart(element, options);
+sc.addEventListener('submit', function handleSubmit(e) {
+    alert('Total: ' + e.total + '€');
+});
 
